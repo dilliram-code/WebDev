5f453176750a0162e04b77f2beb49702ecd2038e
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2023-07-14 20:16:41 +0545

    added

--- practice/script.js
+++ practice/script.js
@@ -1,1 +1,63 @@
-console.log('Hello World')
+"use strict";
+// console.log('Hello World');
+// console.log("JavaScript Practice");
+// console.log(7+8);
+
+
+
+// Let's see the example of template literals
+// let phonePrice = 30000;
+// let earphonePrice = 3000;
+
+// console.log("Total price of the gadgets is: ",(phonePrice+earphonePrice)," Rupees.");
+// console.log("Total price of the gadgets is " + (phonePrice+earphonePrice) + " Rupees.");
+
+// Template literal
+// total = `Total price of the gadgets is: ${phonePrice+earphonePrice} Rupees.`
+// console.log(total);
+
+// let a = 1;
+
+// // Increment operator
+// a++;
+// console.log(a) //2
+
+// a--; 
+// console.log(a) //1
+// console.log(a++) //1
+// console.log(a) //2
+// console.log(a++)
+// console.log(a)
+
+
+// let first_num = 10
+// let second_num = "10"
+// console.log(first_num == second_num)
+// console.log(first_num === second_num)
+
+// console.log(0=="")
+// console.log(1==false)
+// console.log(1===true)
+// console.log(1==true)
+
+
+
+let color1 = "green";
+let color2 = "red";
+let color3 = "yellow";
+
+let value = color1;
+
+if (value==color1)
+{
+    console.log("You can continue your journey");
+} 
+else if (value==color2) 
+{
+    console.log("Danger ahead!! Slow down your speed");
+}else 
+{
+    console.log("Get ready to go!");
+}
+
+

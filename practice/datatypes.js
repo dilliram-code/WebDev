bebb8800137a4df1ca70f2017c51ac8026ab6a51
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2023-07-14 20:46:44 +0545

    data types

---
+++ practice/datatypes.js
@@ -0,0 +1,15 @@
+// Data types in JavaScript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null   => standalone value
+// 5. Undefined
+// 6. BigInt
+// 7. Symbol => unique
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+let value;
+console.log(typeof value);

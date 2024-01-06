8540b2e24ef4b0d1d011b87ae687fdf3889b82f5
Dilli Ram Chaudhary <62086366+Dilli-ram-gif@users.noreply.github.com>
2024-01-06 09:26:58 +0000

    Asynchronous JavaScript

---
+++ practice/primitive_and_object.js
@@ -0,0 +1,104 @@
+'use strict'
+// console.log("This is the session of primitive and object");
+//
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+
+
+
+
+
+//object
+
+// const rabin = {
+//     firstName: "Rabin",
+//     lastName: "Tharu",
+//     age: 27,
+//     location: "Chandragiri"
+// };
+
+// const pawan = rabin;
+// pawan.firstName = 'Chaudhary';
+
+// //see the result
+// console.log(rabin);
+// console.log(pawan);
+
+
+
+
+
+
+
+//primitive and object literals
+// let location = "Kathmandu";
+// let location2 = "Lalitpur";
+// location2 = location;
+
+// console.log(location);
+// console.log(location2);
+
+
+
+
+
+
+
+//Normal function and arrow function
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//         this.sayHi = function () {
+//             console.log("Hi, my name is " + this.name);
+//         };
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//Asynchronous JavaScript
+// console.log("Start");
+
+// // Asynchronous operation: Simulating cooking pasta (takes 2 seconds)
+// setTimeout(() => {
+//   console.log("Pasta is ready!");
+// }, 2000);
+
+// console.log("Continue doing other things...");
+
+// // Asynchronous operation: Simulating chopping vegetables (takes 1 second)
+// setTimeout(() => {
+//   console.log("Vegetables are chopped!");
+// }, 1000);
+
+// console.log("End");
+
+
+
+//My Own code copied from above:
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Pasta is ready!");
+}, 2000);
+
+console.log("Continue doing other things...");
+
+//Asynchronous operation: Simulating chopping vegetables (takes 1 second)
+setTimeout(() => {
+    console.log("Vegetables are chopped!");
+}, 1000)
+  
+console.log("End!");

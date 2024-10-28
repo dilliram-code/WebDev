cee27da9a393107ca6d981aee21646d596a62f95
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-10-28 21:19:06 +0545

    manipulate dom for the form validator

---
+++ Form-Validator-project/script.js
@@ -0,0 +1,9 @@
+let form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function (e) {
+  console.log("Submit");
+})

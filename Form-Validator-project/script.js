0503f400eb1a011708ca05bf1b69dcb1653449c3
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-10-29 08:51:18 +0545

    fix bugs in password match

--- Form-Validator-project/script.js
+++ Form-Validator-project/script.js
@@ -1,28 +1,26 @@
-let form = document.getElementById('form');
-let username = document.getElementById('username');
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let password2 = document.getElementById('password2');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-
-//Show input error message
+// Show input error message
 function showError(input, message) {
-  let formControl = input.parentElement;
+  const formControl = input.parentElement;
   formControl.className = 'form-control error';
-  let small = formControl.querySelector('small');
+  const small = formControl.querySelector('small');
   small.innerText = message;
 }
 
-//Show input success message
-function showSuccess(input, message) {
-  let formControl = input.parentElement;
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-//Check email is valid
+// Check email is valid
 function checkEmail(input) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
@@ -30,9 +28,7 @@ function checkEmail(input) {
   }
 }
 
-
-
-//Check required fields
+// Check required fields
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === '') {
@@ -40,73 +36,45 @@ function checkRequired(inputArr) {
     } else {
       showSuccess(input);
     }
-  })
+  });
 }
 
-//Check input length
+// Check input length
 function checkLength(input, min, max) {
   if (input.value.length < min) {
-    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
   } else if (input.value.length > max) {
-    showError(input, `${getFieldName(input)} must be less than ${max}`)
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
   } else {
     showSuccess(input);
   }
 }
 
-//Check passwords match
-function checkPasswordMatch(input1, input2) {
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
     showError(input2, 'Passwords do not match');
   }
 }
 
-//Get field name
+// Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-
-//Event Listners
+// Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-
-
-  // if (username.value === '') {
-  //   showError(username, "Username is required.")
-  // } else {
-  //   showSuccess(username);
-  // }
-  // if (email.value === '') {
-  //   showError(email, "Email is required.")
-  // } else if (!isValidEmail(email.value)) {
-  //   showError(email, "Email is not valid.")
-  // }
-  // else {
-  //   showSuccess(email);
-  // }
-  // if (password.value === '') {
-  //   showError(password, "Password is required.")
-  // } else {
-  //   showSuccess(password);
-  // }
-  // if (password2.value === '') {
-  //   showError(password2, "Password is required.")
-  // } else {
-  //   showSuccess(password2);
-  // }
-
-
-
-
-  //The code above is not scalable and very inefficient so we are going to replace it with more cleaner code.
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
-  checkEmail(input);
-  checkPasswordMatch(password, password2);
-})
-
-
-//This is upto here only for now, in future definitely I will manage it.
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});

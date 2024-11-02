c960373bf11eaf675262561a2dce057c9fa7b59a
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-02 16:43:47 +0545

    Start a new project Bankist, just for DOM practice

--- SignInSignUP/script.js
+++ SignInSignUP/script.js
@@ -9,3 +9,7 @@ registerBtn.addEventListener('click', () => {
 registerBtn.addEventListener('click', () => {
   container.classList.add('active');
 });
+
+loginBtn.addEventListener('click', () => {
+  container.classList.remove('active');
+})

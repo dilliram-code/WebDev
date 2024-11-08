913e80e3e59a7dcbc7de6c2bd06603f0b1922772
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-08 14:16:11 +0545

    Read attribute of element

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -109,4 +109,7 @@ document.documentElement.style.setProperty(
   '--color-primary', 'orangered'
 );
 
-//
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);

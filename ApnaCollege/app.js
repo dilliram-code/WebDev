b6227b10d97b881b085b1eb8194e1be8427b7324
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-10 18:00:19 +0545

    Log an array using for loop in DOM

--- ApnaCollege/app.js
+++ ApnaCollege/app.js
@@ -13,3 +13,16 @@ console.log(img.src);
 //Manipulate the src of image
 img.src = 'assets/section--1.jpg' //This changes the targeted image
 
+//Get the element by ID
+console.log(document.getElementById('description'));
+
+//Select the element by their classes
+const nameClass = document.getElementsByClassName('oldImg');
+console.log(nameClass);
+
+//Log smallimages
+//Things to note below: smallimages returns an array so we can apply for loop
+let smallimages = document.getElementsByClassName('oldImg');
+for (let i = 0; i < smallimages.length; i++) {
+  console.dir(smallimages[i]);
+}

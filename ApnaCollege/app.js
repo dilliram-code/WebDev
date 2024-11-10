9d268d2fb1501e08178554f357d57ed21e7ab700
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-10 17:44:56 +0545

    Change the src of image

---
+++ ApnaCollege/app.js
@@ -0,0 +1,15 @@
+const  img = document.getElementById("mainImg");
+console.log(img);
+
+//Log the object of img
+console.dir(img);  
+
+//log tagName
+console.log(img.tagName);
+
+//Log the src of image
+console.log(img.src);
+
+//Manipulate the src of image
+img.src = 'assets/section--1.jpg' //This changes the targeted image
+

1587dfc02b440778cb03928f1a15e2634877b490
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-10 10:27:40 +0545

    Move upward in DOM traversing

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -263,8 +263,16 @@ const h1 = document.querySelector('h1');
 //Going downwards: child
 console.log(h1.querySelectorAll('.highlight'));
 console.log(h1.childNodes);
-console.log(h1.parentElement);
 console.log(h1.firstChild)
 console.log(h1.children);
 h1.firstElementChild.style.color = 'white';
 h1.lastElementChild.style.color = 'orangered';
+
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';

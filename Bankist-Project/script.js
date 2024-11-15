f102a14cae02974eeca9386bd7118895708fe623
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-15 20:15:13 +0545

    feat: Add menu fade animation for navigation links and logo

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -129,6 +129,29 @@ tabsContainer.addEventListener('click', function(e){
 })
 
 
+//Menu Fade Animation
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e, opacity){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el =>{
+      if(el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function(e){
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', function(e) {
+  handleHover(e, 1);
+});
 
 ///////////////////////////////////////
 ///////////////////////////////////////

68f39253938367f788c0a5e9d995bcc6c0926326
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-18 21:36:52 +0545

    Feat: Initially hidden section

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -162,6 +162,25 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 
 //Sticky Navigation: Intersection Observer API
+const obsCallback = function(entries, observer){
+  entries.forEach(entry =>{
+    console.log(entry);
+  })
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+}
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
+
+
+
+
+
+
 
 ///////////////////////////////////////
 ///////////////////////////////////////

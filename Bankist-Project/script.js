8ee7145a672dcc8abd3e87fb302184f620eb1b1e
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-19 23:04:06 +0545

    feat: add unobserce the target and code rootMargin for the element

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -234,12 +234,15 @@ const loadImg = function(entries, observer){
 
   entry.target.addEventListener('load', function (){
     entry.target.classList.remove('lazy-img');
-  })
+  });
+
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));

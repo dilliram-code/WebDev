520aa8e463314526111a9129b6540322019daa8c
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-20 19:53:47 +0545

    fix the number of slides

--- Bankist-Project/script.js
+++ Bankist-Project/script.js
@@ -252,6 +252,9 @@ const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
 
+let curSlide = 0;
+const maxSlide = slides.length;
+
 const slider = document.querySelector('.slider');
 slider.style.transform = 'scale(0.4) translateX(-800px)';
 slider.style.overflow = 'visible';
@@ -261,7 +264,18 @@ slides.forEach((s, i) => (s.style.transform = `translateX(${100*i}%)`));
 // 0%, 100%, 200%, 300%
 
 //Next slide 
-btnRight.addEventListener('click', function(){});
+btnRight.addEventListener('click', function(){
+  if(curSlide === maxSlide - 1){
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100*(i - curSlide)}%)`)
+  );
+});
 
 ///////////////////////////////////////
 ///////////////////////////////////////
